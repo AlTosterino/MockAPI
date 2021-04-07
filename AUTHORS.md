920b3426f000f8af0c0e94fba4681e4179ded62c
AlTosterino <altosterino@gmail.com>
2021-04-07 13:07:52 +0200

    Basic structure

--- AUTHORS.md
+++ AUTHORS.md
@@ -1,3 +1,3 @@
-# Contributors
+# Authorr
 
 * AlTosterino <altosterino@gmail.com>
--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
-"""
-    Setup file for mock-api.
-    Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.0.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 from setuptools import setup
 
 if __name__ == "__main__":
